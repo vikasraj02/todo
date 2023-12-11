503eee704decf9c830c0c6479af6f6a750a99e7a
vikasraj02 <vikasraj9143@gmail.com>
2023-12-11 23:40:52 +0530

    Added task to filter is_complete = true task and shown that task in completed task

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,7 +3,9 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_complete = False).order_by("-updated_at")
+    completed_tasks = Task.objects.filter(is_complete = True)
     context = {
         "tasks":tasks,
+        "completed_tasks":completed_tasks,
     }
     return render(request, 'home.html', context)
