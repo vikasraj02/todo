61781b572dcdb0b2c6131492b16756fcb789426d
vikasraj02 <vikasraj9143@gmail.com>
2023-12-14 22:53:53 +0530

    Added functionlity for update and delete in todo app

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("addTask/", views.addTask, name="addTask")
+    #add task
+    path("addTask/", views.addTask, name="addTask"),
+    #mark as done
+    path("mark_as_done/<int:pk>/",views.mark_as_done, name="mark_as_done"),
+    # mark as undone
+    path("mark_as_Undone/<int:pk>/",views.mark_as_Undone, name = 'mark_as_Undone'),
+    # Edit feature
+    path("Edit_task/<int:pk>/", views.Edit_task, name = 'Edit_task'),
+    #Delete feature
+    path("Delete_task/<int:pk>/", views.Delete_task, name = 'Delete_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from .models import Task
 
@@ -7,4 +7,30 @@ from .models import Task
 def addTask(request):
     task = request.POST["task"]
     Task.objects.create(task=task)
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.is_complete = True
+    task.save()
+    return redirect('home')
+
+def mark_as_Undone(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.is_complete = False
+    task.save()
+    return redirect('home')
+def Edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST.get('task')
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {'get_task': get_task}
+        return render(request, 'Edit_task.html',context)
+def Delete_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    get_task.delete()
     return redirect('home')
